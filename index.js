f6ac55564da130dd2f4647cbcee36a024622baa2
Birendra <divyabiru27@gmail.com>
2023-07-06 16:21:38 +0530

    Backend

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express=require("express");
 const colors=require("colors")
+const cors = require('cors');
+const app=express();
+const port=process.env.PORT||8080
+require("dotenv").config()
  const connecttion = require("./config/db");
  const { notFound, errorHandler }=require("./middlewares/errorMiddleware")
+ const authRouter = require("./routes/authRoute");
+ const busRouter = require("./routes/busRoute");
+ const cartRouter = require("./routes/cartRoute");
+ const bookedRouter = require("./routes/bookedRoute");
 
-require("dotenv").config()
-const port=process.env.PORT||8080
-const app=express();
 app.use(express.json())
-const cors = require('cors');
-const authRouter = require("./routes/authRoute");
-const busRouter = require("./routes/busRoute");
-const cartRouter = require("./routes/cartRoute");
-const bookedRouter = require("./routes/bookedRoute");
+
 app.use(cors());
 app.get("/",(req,res)=>{
     res.send("Home page")
 })
 
-app.use("/api/auth",authRouter)
-app.use("/api/bus",busRouter)
-app.use("/api/bus/cart",cartRouter)
-app.use("/api/bus/book",bookedRouter)
+app.use("/auth",authRouter)
+app.use("/bus",busRouter)
+app.use("/cart",cartRouter)
+app.use("/book",bookedRouter)
 
 app.use(notFound)
 app.use(errorHandler)
